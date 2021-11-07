bbd0153883bd28b2659b5e429c5c2271caba8f5c
gustn <gustn@DESKTOP-BOL0KJS>
2021-11-08 02:40:51 +0900

    11/08(월) Todo project : 쿼리만 만드는 곳(로그인 메소드 기능 추가)

---
+++ Todo/src/dao/MemberDao.java
@@ -0,0 +1,43 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import vo.Member;
+
+// 쿼리만 만드는 곳
+public class MemberDao {
+	
+	// (1) 로그인 메소드
+	public Member login(Connection conn, Member paramMember) throws SQLException {
+		// 디버깅 코드
+		System.out.println("[debug] MemberDao : paramMember 확인 -> " + paramMember.toString());
+		
+		// 쿼리 생성
+		String sql = MemberQuery.LOGIN;
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, paramMember.getMemberId());
+		stmt.setString(2, paramMember.getMemberPw());
+		
+		// 쿼리 실행
+		ResultSet rs = stmt.executeQuery();
+		
+		// Member 클래스 객체 변수 선언
+		Member loginMember = null;
+		
+		if(rs.next()) {
+			// Member 클래스 객체 변수에 값 할당
+			loginMember = new Member();
+			loginMember.setMemberId(rs.getString("memberId"));
+			loginMember.setMemberPw(rs.getString("memberPw"));
+		}
+		
+		// 기록 종료
+		rs.close();
+		stmt.close();
+		
+		return loginMember;
+	}
+}
