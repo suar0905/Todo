90d7172d1da47c2618dec9101abe701d31fcfb48
gustn <gustn@DESKTOP-BOL0KJS>
2021-11-08 13:20:35 +0900

    11/08(월) Todo project : 컨트롤러 중 /member로 시작하는 컨트롤러들만 로그인이 되어있지 않으면 로그인 할 수 있도록 필터링

---
+++ Todo/src/filter/LoginFilter.java
@@ -0,0 +1,29 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter("/member/*") // /member로 시작하는 controller들만 가로챔
+public class LoginFilter implements Filter {
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// 로그인 되어있지 않은 상태에서 요청 처리 불가, 즉 로그인 상태에서만 요청할 수 있도록 필터링 됨
+		System.out.println("[debug] LoginFilter : LoginFilter.doFilter() 실행");
+		HttpSession session = ((HttpServletRequest)request).getSession();
+		if(session.getAttribute("loginMember") == null) { // 로그인되어 있지 않은 상태
+			((HttpServletResponse)response).sendRedirect(((HttpServletRequest)request).getContextPath() + "/login");
+			return;
+		}
+		chain.doFilter(request, response);
+	}
+}
